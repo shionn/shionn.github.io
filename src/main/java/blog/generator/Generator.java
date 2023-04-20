7b1c47cf39322273827606b60ff928df397747e2
shionn <shionn@gmail.com>
2023-04-20 20:38:43 +0200

    premier test de generation

--- src/main/java/blog/generator/Generator.java
+++ src/main/java/blog/generator/Generator.java
@@ -1,24 +1,44 @@
 package blog.generator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 public class Generator {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		new Generator().generate();
 	}
 
-	private void generate() {
+	private void generate() throws IOException {
 		TemplateEngine engine = new TemplateEngine();
 		engine.setTemplateResolver(initResolver());
+		engine.process("template", buildContext(), new FileWriter("site/index.html"));
+	}
+
+	private Context buildContext() {
+		Context context = new Context(Locale.FRANCE, buildParam());
+		context.setVariable("test", "ceci est une variable");
+		return context;
+	}
+
+	private Map<String, Object> buildParam() {
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("test", "ceci est un test");
+		return param;
 	}
 
 	private FileTemplateResolver initResolver() {
 		FileTemplateResolver resolver = new FileTemplateResolver();
 		resolver.setTemplateMode(TemplateMode.HTML);
-		resolver.setPrefix("/templates/");
+		resolver.setPrefix("src/main/resources/templates/");
 		resolver.setSuffix(".html");
 		return resolver;
 	}
