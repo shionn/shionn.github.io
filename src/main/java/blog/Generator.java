197511d611aad8f6b47224aca850616eb2d1a250
shionn <shionn@gmail.com>
2024-02-25 00:09:49 +0100

    Extraction del ageneration dans un autre projet

---
+++ src/main/java/blog/Generator.java
@@ -0,0 +1,11 @@
+package blog;
+
+import java.io.IOException;
+
+public class Generator {
+
+	public static void main(String[] args) throws IOException {
+		blog.generator.Generator.main(args);
+	}
+
+}
