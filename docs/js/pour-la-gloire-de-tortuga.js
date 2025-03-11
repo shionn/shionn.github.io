d7b647e56be6bdba21c841368eb73c92f8248348
shionn <shionn@gmail.com>
2025-03-11 10:06:17 +0100

    Update pour-la-gloire-de-tortuga.js

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -431,6 +431,7 @@ q(function() {
 	let q9 = new _quest("quest-9", "Collecter des vivres", "Peindre 20 figurines", 20)
 		.progress("06/03/2025", tony, 3, "Gretchin", _SMALL)
 		.progress("10/03/2025", angest, 3, "Space Machin", _SMALL)
+		.progress("11/03/2025", whisp, 5, "Orcs sur Wargs", _SMALL)
 		.render();
 
 //			
