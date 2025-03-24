e8f71023409594a4fddc8d976e0b6e53e8a55cc4
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-03-24 16:47:13 +0100

    correction tony

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -456,7 +456,7 @@ q(function() {
 
 	let q11 = new _quest("quest-11", "S'emparer d'un navire", "Peindre 50 figurines ou Décors", 50)
 		.progress("23/03/2025", shionn, 18, "Shootas", _SMALL)
-		.progress("24/03/2025", angest, 1, "Une cahouette de l'espace", _SMALL_DECOR)
+		.progress("24/03/2025", tony, 1, "Une cahouette de l'espace", _SMALL_DECOR)
 		.render();
 
 //			
