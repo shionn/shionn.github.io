48a869dd4822e408edad467777aa7b86f791ef4d
shionn <shionn@gmail.com>
2025-03-25 14:00:55 +0100

    Update pour-la-gloire-de-tortuga.js

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -459,6 +459,7 @@ q(function() {
 		.progress("24/03/2025", tony, 1, "Une cahouette de l'espace", _SMALL_DECOR)
 		.progress("24/03/2025", whisp, 1, "Un personnage avec un turban", _SMALL)
 		.progress("25/03/2025", whisp, 1, "Une serveuse", _SMALL)
+		.progress("25/03/2025", whisp, 1, "Une villageois", _SMALL)
 		.render();
 
 //			
