4a30f854a226ab9641bee6b6f786f3b4b2b57b22
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-03-30 16:38:18 +0200

    hutte D&L

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -467,6 +467,8 @@ q(function() {
 		.progress("27/03/2025", whisp, 4, "Coffres", _SMALL_DECOR)
 		.progress("27/03/2025", whisp, 1, "rivière et pont", _BIG_DECOR)
 		.progress("30/03/2025", angest, 1, "Orc Grossache", _SMALL)
+		.progress("30/03/2025", shionn, 1, "Hutte Champignon D&L", _MEDIUM_DECOR)
+		.progress("30/03/2025", shionn, 1, "Hutte Marais D&L", _MEDIUM_DECOR)
 		.render();
 
 //			
