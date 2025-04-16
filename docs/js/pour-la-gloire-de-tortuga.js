9a2934a13d46e40b89a11a6f3f49b0171259d42a
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-04-16 21:08:00 +0200

    dark vador de angest

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -502,6 +502,7 @@ q(function() {
 		.progress("16/04/2025", whisp, 1, "Dalle 6x9", _SMALL_DECOR)
 		.progress("16/04/2025", whisp, 5, "Dalle 3x12", _MEDIUM_DECOR)
 		.progress("16/04/2025", whisp, 5, "Dalle 12x12", _BIG_DECOR)
+		.progress("16/04/2025", angest, 1, "Dark Vadore", _MEDIUM)
 		.render();
 
 		
