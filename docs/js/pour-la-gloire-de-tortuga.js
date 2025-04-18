fb65584df25682357bc91bb390bf1e012eaff35d
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-04-18 22:08:26 +0200

    participation tony et tupad

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -509,6 +509,8 @@ q(function() {
 
 	let q14 = new _quest("quest-14", "création d'une brasserie", "Peindre 50 figurines ou Décors", 50)
 		.progress("17/04/2025", shionn, 2, "Sirène", _SMALL)
+		.progress("18/04/2025", tony, 1, "Gladiateur", _SMALL)
+		.progress("18/04/2025", tupad, 1, "Seigneur Fantom", _MEDIUM)
 		.render();
 		
 //			
