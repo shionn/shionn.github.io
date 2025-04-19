c2d61839de87c94ac38ae2f807fbc102db59f603
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-04-20 00:50:51 +0200

    Angest fox

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -512,6 +512,7 @@ q(function() {
 		.progress("18/04/2025", tony, 1, "Gladiateur", _SMALL)
 		.progress("18/04/2025", tupad, 1, "Seigneur Fantom", _MEDIUM)
 		.progress("19/04/2025", whisp, 4, "Dale exterieur", _MEDIUM_DECOR)
+		.progress("19/04/2025", angest, 1, "Illwyn the Fox", _MEDIUM)
 		.render();
 		
 //			
