a32af06c977cfe5564722dbb1102d5f38435ec53
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-05-14 21:00:15 +0200

    anuabi

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -555,6 +555,7 @@ q(function() {
 		.progress("09/05/2025", angest, 1, "Manticore", _MEDIUM)
 		.progress("09/05/2025", anuabi, 1, "Petit Nounours", _BIG)
 		.progress("14/05/2025", ben, 1, "Silvertower", _SMALL)
+		.progress("14/05/2025", anuabi, 1, "Tyrannide", _SMALL)
 		.render();
 		
 //			
