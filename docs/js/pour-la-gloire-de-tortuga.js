2bf4462af626f89cd568d3a35245b484c10bd89d
shionn <shionn@gmail.com>
2025-05-27 08:23:10 +0200

    Update pour-la-gloire-de-tortuga.js

--- docs/js/pour-la-gloire-de-tortuga.js
+++ docs/js/pour-la-gloire-de-tortuga.js
@@ -579,7 +579,7 @@ q(function() {
 		.progress("21/05/2025", whisp, 8, "Mur 6cm", _SMALL_DECOR)
 		.render();
 
-	let q18 = new _quest("quest-18", "Nous sommes attaqué !", "Peindre 20 figurines en 15 jours", 30)
+	let q18 = new _quest("quest-18", "Nous sommes attaqué !", "Peindre 20 figurines en 15 jours", 20)
 		.progress("22/05/2025", angest, 1, "Seldana sand sorcerer", _SMALL)
 		.progress("22/05/2025", shionn, 1, "chaman tisseur", _SMALL)
 		.progress("23/05/2025", whisp, 1, "Dragon d'or", _MEDIUM)
