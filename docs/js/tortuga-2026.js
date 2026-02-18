8a243d83d51e48e7abce62b4623c844c7ea2c7e0
Kepka Ludovic <shionn@gmail.com>
2026-02-18 21:26:35 +0100

    cristaux tupad

--- docs/js/tortuga-2026.js
+++ docs/js/tortuga-2026.js
@@ -411,11 +411,12 @@ q(function() {
 			.progress("29/01/2026", shionn, 4, "Waytress of Lay", _SMALL)
 			.render();
 
-	let q2 = new _quest("quest-2", "Collecter du bois", "Peindre 40 figurines", 40)
+	let q2 = new _quest("quest-2", "Collecter du bois", "Peindre 40 figurines", 40+15)
 			.progress("29/01/2026", shionn, 2, "Waytress of Lay", _SMALL)
 			.progress("29/01/2026", shionn, 1, "Champion of Lay", _SMALL)
 			.doVideoGameQuest("30/01/2026", transfopaper, 10)
 			.doBugBounty("30/01/2026", marco) // glissade sur le quai
+			.progress("01/02/2026", tupad, 15, "Cristaux SC2", _SMALL_DECOR)
 			.doVideoGameQuest("05/02/2026", cuistotfou, 5)
 			.progress("13/02/2026", tony, 1, "Sony Jona", _SMALL)
 			.progress("13/02/2026", whisp, 7, "Voleurs à dos de fourmis géantes", _MEDIUM)
